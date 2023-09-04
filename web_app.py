03fdd8039b4e3c980ed988bdbc55ae26cf3334b3
Tuyana Chirninova <hikkonki@gmail.com>
2023-09-04 15:10:17 +0300

    a few changes

--- web_app.py
+++ web_app.py
@@ -1,4 +1,15 @@
-<!doctype html>
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
+hostName = 'localhost'
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def __get_html_content(self):
+        return """
+        <!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -434,3 +445,26 @@
 </script>
 </body>
 </html>
+        """
+
+    def do_GET(self):
+        query_components = parse_qs(urlparse(self.path).query)
+        print(query_components)
+        page_content = self.__get_html_content()
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(bytes(page_content, 'utf-8'))
+
+
+if __name__ == '__main__':
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print('Server started http://%s:%s' % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print('Server stopped')
